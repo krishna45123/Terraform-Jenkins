b6d26b73ab9bb472971a0b21e6ceedc1109d9721
krishna45123 <137974689+krishna45123@users.noreply.github.com>
2023-08-22 15:59:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,18 @@
-provider "aws" {
-    region = "us-west-1"  
-}
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-resource "aws_instance" "foo" {
-  count = 3
-  ami           = "ami-0f8e81a3da6e2510a" # us-west-1
-  instance_type = "t2.micro"
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
+
+  aws            = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
   tags = {
-      Name = "TF-Instance"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
-
