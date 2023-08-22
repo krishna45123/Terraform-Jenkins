d0d10719e39897036b969b393101729e2fcead14
krishna45123 <137974689+krishna45123@users.noreply.github.com>
2023-08-22 15:35:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,10 @@
-provider "aws" { 
+provider "aws" {
+    region = "ap-south-1"  
 }
-resource "aws_s3_bucket" "onebucket" {
-   bucket = "testing-s3-with-terraform"
-   acl = "private"
-   versioning {
-      enabled = true
-   }
-   tags = {
-     Name = "Bucket1"
-     Environment = "Test"
-   }
+
+resource "aws_vpc" "foo" {
+  ami           = "ami-0f8e81a3da6e2510a" # ap-south-1
+  tags = {
+      Name = "TF-vpc"
+  }
 }
